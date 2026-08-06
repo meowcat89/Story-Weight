de4ed88aba62798a83d5d711ef8145365385b95e
meowcat89 <164294327+meowcat89@users.noreply.github.com>
2026-08-05 21:59:08 -0400

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import StoryWeight from "./StoryWeight.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <StoryWeight />
+  </React.StrictMode>
+);
